e4b5435516ac205ef1d251057b5159ba641f4990
Nikita Andreev <nik310392illbeback@gmail.com>
2019-10-28 23:00:32 +0300

    Metopt 2 - newton method

--- PyProject1/metopt2.py
+++ PyProject1/metopt2.py
@@ -1,12 +1,10 @@
+import sys
+import time
+import matplotlib.pyplot as plt
 import numpy as np
 import scipy
 import scipy.optimize as opt
-import sklearn
 import sklearn.datasets
-import matplotlib.pyplot as plt
-import sys
-import time
-from scipy.optimize import bracket
 from sklearn.model_selection import train_test_split
 
 
@@ -14,23 +12,17 @@ def sigma(w, x):
     return 1 / (1 + np.exp(-np.dot(w, x)))
 
 
-oracle_counter = 0
-
-
 def function(w, X):
-    global oracle_counter
-    oracle_counter += 1
     return -1 / len(X) * np.sum([np.log(sigma(w, (2 * l - 1) * x)) for x, l in zip(X, labels)])
 
 
 def gradient(w, X):
-    global oracle_counter
-    oracle_counter += 1
     return -1 / len(X) * np.sum([x * (l - sigma(w, x)) for x, l in zip(X, labels)], axis=0)
 
 
 def gessian(w, X):
-    return -1 / len(X) * np.sum([np.outer(x, x) * ((sigma(w, x) - 1) * sigma(w, x)) for x in X], axis=0)
+    sigmas = np.array([sigma(w, x) for x in X])
+    return -1 / len(X) * np.sum([np.outer(x, x) * ((sigmas[i] - 1) * sigmas[i]) for i, x in enumerate(X)], axis=0)
 
 
 def der(fun, point, epsilon=np.sqrt(sys.float_info.epsilon)):
@@ -63,9 +55,9 @@ def check_gessian(grad, gess, R, dim, args=(), diff_eps=np.sqrt(sys.float_info.e
 def golden_search_bounded(fun, a0, b0, eps=0.0001, args=()):
     ratio = (1 + 5 ** 0.5) / 2
 
-    def step(a, b, c, fc):
+    def step(a, b, c, fc, onumber):
         if b - a < eps:
-            return a, fun(a, *args)
+            return a, fun(a, *args), onumber + 1
         else:
             d = a + b - c
             fd = fun(d)
@@ -73,102 +65,155 @@ def golden_search_bounded(fun, a0, b0, eps=0.0001, args=()):
                 c, d = d, c
                 fc, fd = fd, fc
             if fc < fd:
-                return step(a, d, c, fc)
+                return step(a, d, c, fc, onumber + 1)
             else:
-                return step(c, b, d, fd)
+                return step(c, b, d, fd, onumber + 1)
 
     c0 = a0 + (b0 - a0) / ratio
-    return step(a0, b0, c0, fun(c0, *args))[0]
+    solution = step(a0, b0, c0, fun(c0, *args), 0)
+    return solution[0], solution[2]
 
 
 def golden_search(fun, a=0, b=1, eps=0.0001, args=()):
-    x = golden_search_bounded(fun, a, b, eps, args)
+    x, oracle = golden_search_bounded(fun, a, b, eps, args)
     d = b - a
+
     if np.abs(x - a) < eps:
-        return golden_search(fun, a - 10 * d, a, eps, args)
+        ans = golden_search(fun, a - 10 * d, a, eps, args)
+        return ans[0], ans[1] + oracle
     if np.abs(x - b) < eps:
-        return golden_search(fun, b, b + 10 * d, eps, args)
-    return x
+        ans = golden_search(fun, b, b + 10 * d, eps, args)
+        return ans[0], ans[1] + oracle
+
+    return x, oracle
 
 
 def armiho(fun, c=0.5, x0=1, df0=None):
     x = x0
     f0 = fun(0)
+    oracle = 1
+
     if df0 is None:
         df0 = der(fun, 0)
-    while fun(x) > f0 - c * x * df0:
+    while fun(x) > f0 + c * x * df0:
         x /= 2
-    return x
+        oracle += 1
+    oracle += 1
+
+    return x, oracle
 
 
 def nester(fun, f0, d, L0=1):
     L = L0
+    oracle = 0
+
     while fun(1 / L) > f0 - 1 / (2 * L) * np.dot(d, d):
         L *= 2
-    return L
+        oracle += 1
+    oracle += 1
+
+    return L, oracle
 
 
-def gradient_descent(fun, grad, start, one_dim_search='brent', args=(), search_kwargs=dict([]), epsilon=0.0001,
-                     true_min=0):
-    iterations, oracle, times, accuracies, grad_ratios = [], [], [], [], []
-    start_time, k = time.time(), 0
+def solve(G, d):
+    L = np.linalg.cholesky(G)
+    Ltx = scipy.linalg.solve_triangular(L, d, lower=True)
+    return scipy.linalg.solve_triangular(np.transpose(L), Ltx, lower=False)
+
+
+def optimization_task(fun, grad, start, method='gradient descent', gess=None, one_dim_search=None, args=(),
+                      search_kwargs=dict([]), epsilon=0.0001, true_min=0):
+    iterations, oracles, times, accuracies, grad_ratios = [], [], [], [], []
+    start_time, k, oracle = time.time(), 0, 0
     x = start
-    d0 = grad(x, *args)
-    d = d0
-    ratio = 1
+    g0 = grad(x, *args)
+    oracle += 1
+
+    if one_dim_search is None and method == 'gradient descent':
+        one_dim_search = 'brent'
+    elif one_dim_search is None and method == 'newton':
+        one_dim_search = 'unit step'
+
     if one_dim_search == 'nester':
         L, L0 = 2, 0
         if 'L0' in search_kwargs.keys():
             L0 = 2 * search_kwargs['L0']
             L = L0
 
-    while ratio > epsilon:
+    while True:
+        gk = grad(x, *args)
+        fk = fun(x, *args)
+        if method == 'gradient descent':
+            d = -gk
+        elif method == 'newton':
+            d = -solve(gess(x, *args), gk)
+        oracle += 1
+        ratio = np.dot(gk, gk) / np.dot(g0, g0)
         iterations.append(k)
         k += 1
-        oracle.append(oracle_counter)
+        oracles.append(oracle)
         times.append(time.time() - start_time)
-        f0 = fun(x, *args)
-        accuracies.append(f0 - true_min)
+        accuracies.append(fk - true_min)
         grad_ratios.append(ratio)
-        f = lambda alpha: fun(x - d * alpha, *args)
 
-        if one_dim_search == 'golden':
-            alpha = golden_search(f, **search_kwargs)
+        if ratio <= epsilon:
+            break
+
+        f = lambda alpha: fun(x + d * alpha, *args)
+
+        if one_dim_search == 'unit step':
+            alpha = 1
+        elif one_dim_search == 'golden':
+            alpha, oracle_counter = golden_search(f, **search_kwargs)
+            oracle += oracle_counter
         elif one_dim_search == 'brent':
-            alpha = opt.minimize_scalar(f)['x']
+            solution = opt.minimize_scalar(f)
+            alpha = solution['x']
+            oracle += solution['nfev']
         elif one_dim_search == 'armiho':
-            alpha = armiho(f, **search_kwargs, df0=-np.dot(d, d))
+            alpha, oracle_counter = armiho(f, **search_kwargs, df0=np.dot(gk, d))
+            oracle += oracle_counter
         elif one_dim_search == 'wolf':
-            alpha = opt.line_search(fun, grad, x, -d, **search_kwargs, gfk=d, args=args)[0]
+            solution = opt.line_search(fun, grad, x, d, **search_kwargs, gfk=gk, old_fval=fk, args=args)
+            alpha = solution[0]
+            oracle += solution[1]
+
         elif one_dim_search == 'nester':
-            L = nester(f, f0, d, L0=max(L / 2, L0))
+            L, oracle_counter = nester(f, fk, gk, L0=max(L / 2, L0))
             alpha = 1 / L
+            oracle += oracle_counter
 
-        x = x - d * alpha
-        d = grad(x, *args)
-        ratio = np.dot(d, d) / np.dot(d0, d0)
+        x = x + d * alpha
 
-    return x, iterations, oracle, times, accuracies, grad_ratios
+    return x, iterations, oracles, times, accuracies, grad_ratios
 
 
 def polinome(x):
-    return x ** 2
+    return x ** 2 / 2
 
 
 def normalize(array):
     mins = np.array([min(array[:, k]) for k in range(len(array[0]))])
     maxs = np.array([max(array[:, k]) for k in range(len(array[0]))])
-    array = np.array([(vector - mins) / (maxs - mins) for vector in array])
+    array = 0.5 * np.array([(vector - mins) / (maxs - mins) for vector in array])
     return array
 
 
 def graph(x, y, x_l=None, y_l=None, title=None):
     fig, ax = plt.subplots()
     ax.plot(x, [np.log(t) for t in y])
-    ax.set(xlabel=x_l, ylabel=y_l,
-           title=title)
+    ax.set(xlabel=x_l, ylabel=y_l, title=title)
     ax.grid()
     plt.show()
+
+
+def info(w_res, iterations, oracle, times, accuracies, grad_ratios, w_true):
+    print('|x_k - x*| =', np.linalg.norm(w_true - w_res))
+    print('iterations =', iterations[-1])
+    print('oracle =', oracle[-1])
+    print('time =', times[-1])
+    print('accuracy =', accuracies[-1])
+    print('grad_ratio =', grad_ratios[-1])
 
 
 cancer = sklearn.datasets.load_breast_cancer()
@@ -177,8 +222,8 @@ cancer = sklearn.datasets.load_breast_cancer()
 X = normalize(cancer['data'])
 labels = cancer['target']
 X, X_test, labels, labels_test = train_test_split(X, labels, test_size=0.2, shuffle=False)
-print(check_gradient(function, gradient, 1, X.shape[1], args=[X]))
-print(check_gessian(gradient, gessian, 1, X.shape[1], args=[X]))
+# print(check_gradient(function, gradient, 1, X.shape[1], args=[X]))
+# print(check_gessian(gradient, gessian, 1, X.shape[1], args=[X]))
 
 w0 = np.array([0.52658227, 0.37548235, 0.27720264, 0.82988368, 0.63424511, 0.34688084
                   , 0.05520993, 0.15297424, 0.00745145, 0.08471717, 0.5714892, 0.62753455
@@ -187,6 +232,8 @@ w0 = np.array([0.52658227, 0.37548235, 0.27720264, 0.82988368, 0.63424511, 0.346
                   , 0.20133476, 0.82547662, 0.843942, 0.87022425, 0.1425544, 0.16122998]
               )
 # w0 = np.random.random(30)
+
+
 w_true = np.array([302.5925661, -21.69111231, 207.55842006, -411.28849642, -32.24866798
                       , -4.31827468, -34.71080755, -73.71085179, 38.02308458, 49.91720216
                       , 66.57753268, 15.52855102, 27.0831392, -312.8996407, 24.89967549
@@ -195,16 +242,21 @@ w_true = np.array([302.5925661, -21.69111231, 207.55842006, -411.28849642, -32.2
                       , 79.36381994, -51.81633846, 19.5727145, -65.71161459, -102.40926345])
 print(np.mean(np.array([round(sigma(w_true, x)) for x in X_test]) == labels_test))
 start = time.time()
-w_res, iterations, oracle, times, accuracy, grad_ratio = gradient_descent(function,
-                                                                          gradient, w0, args=[X],
-                                                                          one_dim_search='nester',
-                                                                          # search_kwargs=dict(
-                                                                          #     [('c', 0.5), ('x0', 10)]),
-                                                                          epsilon=0.000001,
-                                                                          true_min=function(w_true, X))
-print(np.linalg.norm(w_res - w_true))
+w_res, iterations, oracles, times, accuracies, grad_ratios = optimization_task(function,
+                                                                               gradient, w0,
+                                                                               method='newton',
+                                                                               gess=gessian,
+                                                                               args=[X],
+                                                                               one_dim_search='nester',
+                                                                               # search_kwargs=dict(
+                                                                               # [('c', 0.5), ('x0', 10)]),
+                                                                               # [('maxiter', 10)]),
+                                                                               epsilon=0.00001,
+                                                                               true_min=function(w_true, X))
+
 print(np.mean(np.array([round(sigma(w_res, x)) for x in X_test]) == labels_test))
-print(time.time() - start)
+print(np.linalg.norm(w_res))
+info(w_res, iterations, oracles, times, accuracies, grad_ratios, w_true)
 # graph(iterations[5:], accuracy[5:], title='1.1')
 # graph(times[5:], accuracy[5:], title='2.1')
 # graph(oracle[5:], accuracy[5:], title='3.1')
