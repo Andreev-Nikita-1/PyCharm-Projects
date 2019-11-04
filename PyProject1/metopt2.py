8a1b72e485f70666b6c5f6d1ba9edb0784f53280
Nikita Andreev <nik310392illbeback@gmail.com>
2019-11-04 17:54:03 +0300

    Metopt 2 - script

--- PyProject1/metopt2.py
+++ PyProject1/metopt2.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import matplotlib.pyplot as plt
+import pylab
 import numpy as np
 import scipy
 import scipy.optimize as opt
@@ -33,12 +34,12 @@ def sigmoid(x):
     return ans
 
 
-def oracle(w, X, labels, outers=None, order=0, grad_only=False):
+def oracle(w, X, labels, outers=None, order=0, no_function=False):
     Xw = X.dot(w)
     sigmoids = [sigmoid(l * xw) for xw, l in zip(Xw, labels)]
 
     f = 0
-    if not grad_only:
+    if not no_function:
         f = -1 / X.shape[0] * np.sum([np.log(s) for s in sigmoids])
 
     if order == 0:
@@ -52,6 +53,7 @@ def oracle(w, X, labels, outers=None, order=0, grad_only=False):
         return f, g, 0
 
     hess_coeffs = np.array([s * (1 - s) for s in sigmoids])
+    # outers - массив x x^t flatten, который можно посчитать заранее
     if outers is None:
         h = 1 / X.shape[0] * np.sum([np.outer(x, x) * hess_coeffs[i] for i, x in enumerate(X.todense())], axis=0)
     else:
@@ -134,7 +136,7 @@ def golden_search(fun, eps=sys.float_info.epsilon, args=()):
     return gsb[0], gsb[1] + onumber
 
 
-def armiho(fun, c=0.1, k=10, f0=None, df0=None):
+def armiho(fun, c=0.1, k=20, f0=None, df0=None):
     x = 1
     oracle = 0
 
@@ -142,6 +144,7 @@ def armiho(fun, c=0.1, k=10, f0=None, df0=None):
         f0 = fun(0)
         oracle += 1
 
+    print('--f0', fun(0))
     if df0 is None:
         df0 = der(fun, 0)
         oracle += 2
@@ -151,6 +154,7 @@ def armiho(fun, c=0.1, k=10, f0=None, df0=None):
     oracle += 2
 
     while True:
+        print(x, fx, f0 + c * df0 * x)
         if fx > f0 + c * df0 * x:
             x /= k
             fkx = fx
@@ -159,24 +163,26 @@ def armiho(fun, c=0.1, k=10, f0=None, df0=None):
         elif fkx < f0 + k * c * df0 * x:
             x *= k
             fx = fkx
-            fkx = fun(x)
+            fkx = fun(k * x)
             oracle += 1
         else:
             break
-    print(f0, "a>", fx)
-    return x, oracle
+    print('---returning', fx)
+    return x, fx, oracle
 
 
 def nester(fun, f0, d, L0=1):
     L = L0
     oracle = 0
-
-    while fun(1 / L) > f0 - 1 / (2 * L) * np.dot(d, d):
-        L *= 2
-        oracle += 1
+    fx = fun(1 / L)
     oracle += 1
 
-    return L, oracle
+    while fx > f0 - 1 / (2 * L) * np.dot(d, d):
+        L *= 2
+        fx = fun(1 / L)
+        oracle += 1
+
+    return L, fx, oracle
 
 
 def solve(G, d):
@@ -205,6 +211,7 @@ def optimization_task(oracle, start, method='gradient descent', one_dim_search=N
     f0, g0, _ = oracle(x, *args, order=1)
     fc += 1
     gc += 1
+    fk = f0
 
     if one_dim_search == 'nester':
         L, L0 = 2, 0
@@ -212,18 +219,23 @@ def optimization_task(oracle, start, method='gradient descent', one_dim_search=N
             L0 = 2 * search_kwargs['L0']
             L = L0
 
+    flag = one_dim_search == 'armiho' or one_dim_search == 'wolf' or one_dim_search == 'nester'
+
     while True:
-        fk, gk, hk = oracle(x, *args, order=ord)
-        fc += 1
+        f_, gk, hk = oracle(x, *args, order=ord, no_function=flag)
+        # в реализации в main.py стоит флаг no_function
+        if not flag:
+            fk = f_
+            fc += 1
         gc += 1
         if ord == 2:
             hc += 1
+
         if method == 'gradient descent':
             d = -gk
         elif method == 'newton':
             d = -solve(hk, gk)
-        # elif solver == 'cg':
-        #     d = -scipy.sparse.linalg.cg(hk, gk)[0]
+
         ratio = np.dot(gk, gk) / np.dot(g0, g0)
         iterations.append(k)
         k += 1
@@ -249,24 +261,29 @@ def optimization_task(oracle, start, method='gradient descent', one_dim_search=N
             alpha = solution['x']
             fc += solution['nfev']
         elif one_dim_search == 'armiho':
-            alpha, oracle_counter = armiho(fun, **search_kwargs, f0=fk, df0=np.dot(gk, d))
-            print(fk, "->", fun(alpha))
+            print('---fk', oracle(x, *args))
+            print('--in', fk)
+            alpha, fk, oracle_counter = armiho(fun, **search_kwargs, f0=fk, df0=np.dot(gk, d))
+            print('--out', fk)
+
             fc += oracle_counter
         elif one_dim_search == 'wolf':
             f_for_wolf = lambda z: oracle(z, *args)
-            g_for_wolf = lambda z: oracle(z, *args, order=1, grad_only=True)[1]
+            g_for_wolf = lambda z: oracle(z, *args, order=1, no_function=True)[1]
             solution = opt.line_search(f_for_wolf, g_for_wolf, x, d, **search_kwargs, gfk=gk, old_fval=fk)
             alpha = solution[0]
+            if alpha == None:
+                alpha = 1
             fc += solution[1]
             gc += solution[2]
+            fk = solution[3]
         elif one_dim_search == 'nester':
-            L, oracle_counter = nester(fun, fk, gk, L0=max(L / 2, L0))
+            L, fk, oracle_counter = nester(fun, fk, gk, L0=max(L / 2, L0))
             alpha = 1 / L
             fc += oracle_counter
 
         x = x + d * alpha
 
-    print('-', end='')
     return x, iterations, times, values, grad_ratios, fcalls, gcalls, hcalls
 
 
@@ -348,7 +365,7 @@ outers_rand = scipy.sparse.csr_matrix([np.outer(x, x).flatten() for x in X_rand.
 # X = X_cancer
 # labels = labels_cancer
 # X, X_test, labels, labels_test = train_test_split(X, labels, test_size=0.1)
-# w0_cancer = (2 * np.random.random(X_cancer.shape[1]) - 1) / 2
+w0_cancer = (2 * np.random.random(X_cancer.shape[1]) - 1) / 2
 # ww, _, _, t1, _, _, _, _ = optimization_task(oracle, w0_cancer, method='newton',
 #                                              args=[X_cancer, labels_cancer, outers_cancer], epsilon=1e-8)
 # Xw = X_test.dot(ww)
@@ -380,10 +397,49 @@ for i in range(X.shape[1]):
 # print(check_gradient(function, gradient, 2, X.shape[1], args=[X, labels]))
 # print(check_hessian(gradient, hessian, 2, X.shape[1], args=[X, labels]))
 # exit(0)
+wcc = np.array([4.51749463e-01, 1.52952033e-01, 3.00656181e-01, 9.03358699e-02
+                   , 6.40433491e-01, 4.37594457e-02, 1.98093133e-02, 7.53866494e-01
+                   , 4.88037124e-01, -8.32214664e-01, 2.11683256e+01])
+print(oracle(wcc, X_cancer, labels_cancer))
+exit(0)
 
 w0 = (2 * np.random.random(X.shape[1]) - 1) / 2
 w0_a1a = (2 * np.random.random(X_a1a.shape[1]) - 1) / 2
+X = scipy.sparse.csr_matrix(X_a1a)
+labels = labels_a1a
+w0 = scipy.sparse.csr_matrix(w0_a1a)
 
+ind = [X[:, i].sum() for i in range(X.shape[1])]
+offset = 0
+for i in range(X.shape[1]):
+    if ind[i] < 1:
+        X1 = X[:, :(i - offset)]
+        X2 = X[:, (i - offset + 1):]
+        X = scipy.sparse.csr_matrix(scipy.sparse.hstack([X1, X2]))
+        w0 = scipy.sparse.csr_matrix(scipy.sparse.hstack([w0[:, :(i - offset)], w0[:, (i - offset + 1):]]))
+        offset += 1
+a1, i1, t1, v1, r1, fc1, gc1, hc1 = optimization_task(oracle, w0_cancer, method='newton',
+                                                      args=[X_cancer, labels_cancer, outers_cancer],
+                                                      one_dim_search='wolf', max_time=0.1,
+                                                      search_kwargs=dict([('c2', 0.9)]))
+a2, i2, t2, v2, r2, fc2, gc2, hc2 = optimization_task(oracle, w0_cancer, method='newton',
+                                                      args=[X_cancer, labels_cancer, outers_cancer],
+                                                      one_dim_search='wolf', max_time=0.1,
+                                                      search_kwargs=dict([('c2', 0.5)]))
+a3, i3, t3, v3, r3, fc3, gc3, hc3 = optimization_task(oracle, w0_cancer, method='newton',
+                                                      args=[X_cancer, labels_cancer, outers_cancer],
+                                                      one_dim_search='wolf', max_time=0.1,
+                                                      search_kwargs=dict([('c2', 0.3)]))
+a4, i4, t4, v4, r4, fc4, gc4, hc4 = optimization_task(oracle, w0_cancer, method='newton',
+                                                      args=[X_cancer, labels_cancer, outers_cancer],
+                                                      one_dim_search='wolf', max_time=0.1,
+                                                      search_kwargs=dict([('c2', 0.1)]))
+a5, i5, t5, v5, r5, fc5, gc5, hc5 = optimization_task(oracle, w0_cancer, method='newton',
+                                                      args=[X_cancer, labels_cancer, outers_cancer],
+                                                      one_dim_search='wolf', max_time=0.1,
+                                                      search_kwargs=dict([('c2', 0.001)]))
+
+exit(0)
 # начальная точка
 
 # w_true_a1a = opt.minimize(function, w0_a1a, args=(X_a1a, labels_a1a), jac=gradient)['x']
@@ -408,24 +464,27 @@ w0_cancer = (2 * np.random.random(X_cancer.shape[1]) - 1) / 5
 # f_min_rand = function(w_true_rand, X_rand, labels_rand)
 
 
-def graph_several(xs, ys, labels, x_l=None, y_l=None, title=None):
-    fig, ax = plt.subplots()
-
-    end = min([max(x) for x in xs])
-    inds = [np.argmin([np.abs(p - end) for p in x]) for x in xs]
-    xs1 = [xs[i][:inds[i]] for i in range(len(xs))]
-    ys1 = [ys[i][:inds[i]] for i in range(len(xs))]
+def graph_several(xs, ys, labels, end=None, beg=0, x_l=None, y_l=None, title=None):
+    pylab.rcParams['figure.figsize'] = 1, 1
+    pylab.subplot(1, 2, 1)
+    if end is None:
+        end = max([max(x) for x in xs])
+    ends = [np.argmin([np.abs(p - end) for p in x]) for x in xs]
+    begs = [np.argmin([np.abs(p - beg) for p in x]) for x in xs]
+    xs1 = [xs[i][begs[i]:ends[i] + 1] for i in range(len(xs))]
+    ys1 = [ys[i][begs[i]:ends[i] + 1] for i in range(len(xs))]
 
     def logs(y):
         return [np.log(v) for v in y]
 
     for i in range(len(xs)):
-        ax.plot(xs1[i], ys1[i], label=labels[i])
+        pylab.plot(xs1[i], logs(ys1[i]), label=labels[i])
 
-    ax.set(xlabel=x_l, ylabel=y_l, title=title)
-    ax.grid()
-    ax.legend(fontsize=20)
-    plt.show()
+    pylab.title(title)
+    pylab.grid()
+    # pylab.legend(fontsize=20)
+    pylab.legend()
+    pylab.show()
 
 
 def info(w_res, iterations, oracle, times, accuracies, grad_ratios, w_true):
