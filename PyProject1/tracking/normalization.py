e3f5c013529573c2a9f1ca1aeec58c37e2cf9d87
Nikita Andreev <nik310392illbeback@gmail.com>
2020-02-03 18:42:08 +0300

    nir

--- PyProject1/tracking/normalization.py
+++ PyProject1/tracking/normalization.py
@@ -3,20 +3,20 @@ import json
 import Levenshtein
 from tracking.beam_search import *
 import os
+import tensorflow.compat.v1 as tf
 
-service = 'ems'
+tf.disable_v2_behavior()
 
 
-def index_checking(numbers_str):
+def index_checking(numbers_str, service='ems'):
     if service == 'ems':
         return True
-    else:
-        index = numbers_str[:6]
-        post_inds = np.load("post_indices.npy")
-        return index in post_inds
+    index = numbers_str[:6]
+    post_inds = np.load("post_indices.npy")
+    return index in post_inds
 
 
-def control_number_checking(numbers_str):
+def control_number_checking(numbers_str, service='ems'):
     numbers = [int(s) for s in numbers_str]
     if service == 'ems':
         return control_number_ems(numbers[:-1]) == numbers[-1]
@@ -40,7 +40,7 @@ def control_number_ems(numbers):
     return result
 
 
-def numbers_norm(numbers_words):
+def numbers_norm(numbers_words, service='ems'):
     with open("words_to_numbers.json", encoding='utf-8') as f:
         words_to_num = json.loads(f.read())
     words_nums = list(words_to_num.keys())
@@ -94,97 +94,6 @@ def reducing(numbers, expected_length):
     reducing_numbers = [reducing_number(n, m) for n, m in zip(numbers, numbers[1:])]
     subs = subsets(reducing_numbers, diff)
     return [reduce(numbers, r_ns + [0]) for r_ns in subs]
-
-
-# def por_answers(start=0, end=20):
-#     for i in range(start, end):
-#         v = np.load('data/Por_probs/' + str(i + 1) + '.npy')
-#         print(numbers_norm(beam_search(v)))
-
-
-def por():
-    all = 0
-    cor = 0
-    len_ = 0
-    len_cor = 0
-    with open("data/por_keys.json", encoding='utf-8') as d:
-        keys = json.loads(d.read())
-    folders = os.listdir('data/Por_toloka')
-    for folder in folders:
-        files = os.listdir('data/Por_toloka/' + folder)
-        correct = 0
-        for file in files:
-            v = np.load('data/Por_toloka/' + folder + '/' + file)
-            id = file[:-4]
-            beam = beam_search(v, k=1)
-
-            ans = numbers_norm(beam.split())
-            len_ += len(ans)
-            if keys[id] in ans:
-                correct += 1
-                len_cor += len(ans)
-            else:
-                print('    ', beam, '\n     ', keys[id], len(ans), ans, id)
-
-        print(correct, '/', len(files), '  :   ', folder)
-        cor += correct
-        all += len(files)
-    print('all: ', cor, '/', all, '       len: ', len_ / all, '    len cor: ', len_cor / cor)
-
-
-def ems():
-    all = 0
-    cor = 0
-    len_ = 0
-    lens_ = 0
-    len_cor = 0
-    lens_cor = 0
-    cort = 0
-    corn = 0
-    corc = 0
-    with open("data/ems_keys.json", encoding='utf-8') as d:
-        keys = json.loads(d.read())
-    folders = os.listdir('data/Ems_toloka')
-    for folder in folders:
-        files = os.listdir('data/Ems_toloka/' + folder)
-        print('    ', folder)
-        correct = 0
-        for file in files:
-            v = np.load('data/Ems_toloka/' + folder + '/' + file)
-            id = file[:-4]
-            beam = beam_search(v, k=5)
-            # ans_lens = ems_norm(beam)
-            ts, ns, cs = ems_norm(beam)
-            # ans = ans_lens[0]
-            # lens = ans_lens[1]
-            # len_ += len(ans)
-            # lens_ += lens
-            real = keys[id]
-            if real[:2] in ts and real[2:11] in ns and real[-2:] in cs:
-                correct += 1
-
-                # len_cor += len(ans)
-                # lens_cor += lens
-            else:
-                print(len(ts), len(ns), len(cs))
-                print(beam)
-                if real[:2] not in ts:
-                    cort += 1
-                    print('t', end=" ")
-                if real[2:11] not in ns:
-                    print('n', end=" ")
-                    corn += 1
-                if real[-2:] not in cs:
-                    print('c', end=" ")
-                    corc += 1
-                print(real, ts, ns, cs)
-                # print('    ', beam, '\n     ', keys[id], lens, len(ans), ans, id)
-        # print(correct, '/', len(files), '  :   ', folder)
-        cor += correct
-        all += len(files)
-    # print('all: ', cor, '/', all, '       len: ', len_ / all, '    len cor: ', len_cor / cor, "   lens: ", lens_ / all,
-    #       "   lens cor: ", lens_cor / all)
-    print(cort, corn, corc, cor, ' / ', all)
 
 
 def type_norm(words):
@@ -244,89 +153,59 @@ def ems_norm(beam):
     ts = type_norm(type_words)
     ns = numbers_norm(numbers_words)
     cs = country_norm(country_words)
-    # return [t + n + c for t in ts for n in ns for c in cs], np.array([len(ts), len(ns), len(cs)])
     return ts, ns, cs
 
 
-def control():
-    correct = ["CA007433791SK(1).npy", "CA007433791SK.npy", "CA032156295RU.npy", "CA460681341AT(1).npy",
-               "CA460681341AT.npy", "CB001324705RU.npy", "CC001848257AM(1).npy", "CC001848257AM.npy",
-               "CC015274068ES.npy", "CC015308345ES(1).npy", "CC015308345ES.npy", "CC015308345IC(1).npy",
-               "CC015308345IC.npy", "CC060510876IL.npy", "CC066204605IL.npy", "CD389708309JP.npy",
-               "CD532874587NL(1).npy", "CD532874587NL.npy", "CE384308975BE(1).npy", "CE384308975BE.npy",
-               "CF190999356DE(1).npy", "CF190999356DE.npy", "CG002327031IS(1).npy", "CG002327031IS(2).npy",
-               "CG002327031IS.npy", "CG014604718DE.npy", "CG103316599LT(1).npy", "CG103316599LT.npy",
-               "CH080033447US.npy", "CH081445532US.npy", "CH108815211AU(1).npy", "CH108815211AU.npy",
-               "CJ003862203RU.npy", "CK068015119DE.npy", "CL001191142RU.npy", "CL056436176JP.npy",
-               "CO821284605DE(1).npy", "CO821284605DE.npy", "CV033885375CZ(1).npy", "CV033885375CZ.npy",
-               "CX427163343US.npy", "CY114763116US(1).npy", "CY114763116US.npy", "EE005074800RU.npy",
-               "EE790557156TW(1).npy", "EE790557156TW.npy", "EF014887886RU(1).npy", "EF014887886RU(2).npy",
-               "EF014887886RU.npy", "EH004852600US.npy", "EP056592600RU.npy", "EP073906229RU.npy",
-               "EW001430412IT(1).npy", "EW001430412IT.npy", "EZ188779657US(2).npy", "EZ188779657US.npy",
-               "LG618690024GB.npy", "LO122677706CN.npy", "LS824805509CH(2).npy", "LS824805509CH.npy",
-               "LS890460115CH.npy", "LZ432379988US(1).npy", "LZ432379988US.npy", "LZ497696959IC(1).npy",
-               "LZ497696959IC.npy", "RA019477640RU.npy", "RA253321807FI.npy", "RA259640517FI.npy", "RA612153932UA.npy",
-               "RB012363705RU.npy", "RB601208140SG(1).npy", "RB601208140SG.npy", "RB791356155SG(2).npy",
-               "RB791356155SG.npy", "RC092107717IT(1).npy", "RC092107717IT.npy", "RC727913178RG.npy",
-               "RD009172569HK.npy", "RD213349651SE.npy", "RD331395160IN.npy", "RE540161296UA.npy", "RE684604472GR.npy",
-               "RF020723158UA.npy", "RG973774831CN.npy", "RP019120314NL.npy", "RP900199158SG.npy", "RQ004050118RU.npy",
-               "RQ020905632CY.npy", "RQ108337176UZ.npy", "RQ108337176UZ9(1).npy", "RR000396224GE.npy",
-               "RR061424096BY.npy", "RR170116017TH.npy", "RR321763837PL.npy", "RS002497336MN.npy",
-               "RU480548222HK(2).npy", "RU480548222HK.npy", "RV168615668CN.npy", "RV199755489CN(2).npy",
-               "RV199755489CN.npy", "RX422387137CN.npy", "RY009826924HK(1).npy", "RY009826924HK(2).npy",
-               "RY009826924HK.npy", "RZ022677306LV.npy", "UA789445529HK.npy", "UB082931076HK.npy", "UC460345146YP.npy",
-               "UR521770422CN.npy", "VI629577860CN.npy", "ZA502560528LV(2).npy", "ZA502560528LV.npy",
-               "ZA523577679LV.npy", "ZB020503497HK.npy", "ZB021478900HK(1).npy", "ZB021478900HK.npy",
-               "ZC008498768HK.npy", "LL136789702CN.npy", "RP731131053CN.npy", "LL136789702CN(2).npy",
-               "RP677676920CN.npy", "RA010477280JP.npy", "RP731131053CN(1).npy", "LO122677706CN(2).npy",
-               "LC004077464CN.npy", "za669962194hk.npy", "UH879243837WS.npy", "za669962194hk(2).npy",
-               "CT616989794CN(2).npy", "UH879243837WS(2).npy", "ZA123456789HK.npy", "CT616989794CN.npy"]
-    folder = 'data/control_set'
-    files = os.listdir(folder)
-    out = open("data/control_out.txt", "w")
-    tr, fs = 0, 0
-    for file in set(files) - set(correct):
-        real = file[:13].upper()
-        nb = [int(s) for s in real[2:11]]
-        if not control_number_ems(nb[:-1]) == nb[-1]:
-            out.write('\"' + file + '\"' + ',')
-            continue
-        mat = np.load(folder + '/' + file)
-        beam = beam_search(mat)
-        letters = get_letters() + ['|']
-
-        ts, ns, cs = ems_norm(beam)
-        try:
-            ans = real[:2] in ts and real[2:11] in ns and real[-2:] in cs
-            # out.write(real + ' ' + ts[0] + ns[0] + cs[0] + '\n')
-            if ans:
-                out.write('\"' + file + '\"' + ',')
-                tr += 1
-            else:
-                print()
-                if real[:2] not in ts:
-                    print('t', end=' ')
-                if real[2:11] not in ns:
-                    print('n', end=' ')
-                if real[-2:] not in cs:
-                    print('c')
-                print("".join([letters[np.argmax(p)] for p in mat if letters[np.argmax(p)] != '']))
-                print(beam)
-                # print(ans, real)
-                fs += 1
-                print(real, ts, ns, cs)
-        except:
-            fs += 1
-            print(real, ts, ns, cs)
-    # print(tr / (tr + fs))
-
-#
-#
-#
+def str_to_inds(st):
+    letters = get_letters()
+    return [letters.index(s) for s in st]
 
 
-#
-#
+def show(matr):
+    letters = get_letters()
+    print("".join([letters[np.argmax(p)] for p in matr if letters[np.argmax(p)] != '']))
+
+
+mat_ = np.load('test_mat.npy')
+
+
+def ctc_loss(matr, st):
+    inds = str_to_inds(st)
+    graph = tf.Graph()
+    mat1 = matr.reshape(-1, 1, matr.shape[1])
+    with graph.as_default():
+        labels = tf.sparse_placeholder(tf.int32, [1, len(st)])
+        inputs = tf.placeholder(tf.float32, [matr.shape[0], 1, matr.shape[1]])
+        sequence_length = tf.placeholder(tf.int32, [1])
+        # labels_ = tf.sparse.from_dense(labels)
+        # inputs_ = tf.convert_to_tensor(labels)
+        # sequence_length_ = tf.convert_to_tensor(sequence_length)
+        ctc_loss1 = tf.nn.ctc_loss(labels=labels, inputs=inputs, sequence_length=sequence_length)
+    labels_ = tf.constant(np.array(inds))
+    inputs_ = tf.constant(mat1)
+    sequence_length_ = tf.constant(np.array([len(st)]))
+    feed = {labels: labels_,
+            inputs: inputs_,
+            sequence_length: sequence_length_}
+    with tf.Session() as sess:
+        return sess.run([ctc_loss1], feed_dict=feed)
+
+
+ctc_loss(mat_, 'а')
+# x = tf.placeholder(tf.float32, [2, 2])
+# y = tf.placeholder(tf.float32, [2, 3])
+# z = tf.linalg.matmul(x, y)
+# a = np.array([[0, 1], [1, 0]])
+# a = tf.convert_to_tensor(np.array([[0, 1], [1, 0]]))
+# b = tf.convert_to_tensor(np.array([[1, 2, 3], [5, 6, 7]]))
+# b = np.array([[1, 2, 3], [5, 6, 7]])
+# feed = {x: a, y: b}
+# with tf.Session() as sess:
+#     ans = sess.run([z], feed_dict=feed)
+# print(ans)
+
+# with
+# (
 #
 #
 #
